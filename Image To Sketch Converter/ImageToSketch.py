64115f9c685ff87308d8a4893f0e3d6c49253dbf
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-26 17:33:10 +0530

    image to sketch maker

---
+++ Image To Sketch Converter/ImageToSketch.py
@@ -0,0 +1,14 @@
+import cv2
+import os
+
+os.chdir('C:/Users/KIIT/Desktop/ALL CODE ND STUFFS/PROJECTS-REPOS/Python Folder/20-beginner-python-projects/Image To Sketch Converter')
+# print(dir)
+image = cv2.imread('tommy.jpg')
+
+grey_img = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+invert = cv2.bitwise_not(grey_img)
+blur = cv2.GaussianBlur(invert,(21,21),0)
+inverted_blur = cv2.bitwise_not(blur)
+sketch = cv2.divide(grey_img,inverted_blur,scale=256.0)
+
+cv2.imwrite('tommysketch.png',sketch)
