8c51d70e00aa96e540452a4ea67bd84a8353019b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-11-26 18:21:43 +0530

    designed the program to read out the whole pdf

--- PDF Text to Voice/PDF_text_to_voice_converter.py
+++ PDF Text to Voice/PDF_text_to_voice_converter.py
@@ -5,14 +5,18 @@ pdf_file_obj = open("C:/Users/KIIT/Desktop/KIIT_me/SEM 5/CN/week 11.pdf",'rb')
 
 pdf_file_reader = PyPDF2.PdfFileReader(pdf_file_obj)
 
-page_obj = pdf_file_reader.getPage(2)
-
-page_text = page_obj.extract_text()
-
-print(page_text)
-
+page = 1
 speak_obj = pyttsx3.init()
-speak_obj.say(page_text)
-speak_obj.runAndWait()
+while page <= pdf_file_reader.numPages:
+    page_obj = pdf_file_reader.getPage(page)
+
+    page_text = page_obj.extract_text()
+
+    # print(page_text)
+    speak_obj.say(page_text)
+    speak_obj.runAndWait()
+    speak_obj.say('Page complete, moving to next page')
+    speak_obj.runAndWait()
+    page += 1
 
 pdf_file_obj.close()
